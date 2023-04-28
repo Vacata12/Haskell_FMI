731901dd73bfb23b67cfd911b582db7f7ee0007c
Ivan Stoynev <79401987+Vacata12@users.noreply.github.com>
2023-04-28 16:24:13 +0300

    Update Task02.hs

--- For_Home_08/Task02.hs
+++ For_Home_08/Task02.hs
@@ -1,11 +1,13 @@
 import Data.List
 main :: IO()
 main = do
-    print $ listLeaves [(1, 2, 3), (2, 4, 5)] -- == [4, 3, 5]
-    print $ listLeaves [(2, 4, 5), (1, 2, 3)] -- == [4, 5, 3]
-    print $ listLeaves [(1, 2, 3), (3, 4, 5), (5, 6, 9)] -- == [2, 4, 6, 9]
+    print $ listLeaves [(1, 2, 3), (2, 4, 5)]  == [4, 3, 5]
+    print $ listLeaves [(2, 4, 5), (1, 2, 3)]  == [4, 5, 3]
+    print $ listLeaves [(1, 2, 3), (3, 4, 5), (5, 6, 9)]  == [2, 4, 6, 9]
 
-listLeaves :: [(Int -> Int -> Int)] -> [Int -> Int]
-listLeaves [] = []
-listLeaves [(x,y,z)] = [y,z]
-listLeaves [()]
+getAllNum :: [(Int, Int, Int)] -> [Int]
+getAllNum [] = []
+getAllNum gr = [y | (_, y, _) <- gr] ++ [z | (_, _, z) <- gr]
+
+listLeaves :: [(Int, Int, Int)] -> [Int]
+listLeaves gr = [a | a <- getAllNum gr, notElem a [x | (x, _, _) <- gr]]
