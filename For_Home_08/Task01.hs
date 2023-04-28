5a3cdbad69a853d857485acbe8d35671a19ce09b
Ivan Stoynev <79401987+Vacata12@users.noreply.github.com>
2023-04-28 16:25:02 +0300

    Update Task01.hs

--- For_Home_08/Task01.hs
+++ For_Home_08/Task01.hs
@@ -5,10 +5,10 @@ type Path = [Node]
 
 main :: IO()
 main = do
-    print $ subsequences [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] -- 0 1 -- == [[1]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1 -- == [[1, 2], [1, 3]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1 -- == [[1, 2, 3], [1, 2, 4]]
-    -- print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2 -- == [[2,3],[2,4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 0 1  == [[1]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 1  == [[1, 2], [1, 3]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 2 1  == [[1, 2, 3], [1, 2, 4]]
+    print $ simplePaths [(1, [2, 3]), (2, [3, 4]), (3, []), (4, [])] 1 2  == [[2,3],[2,4]]
 
 
 getChildren :: Graph -> Node -> [Node]
@@ -19,4 +19,4 @@ isPath g path = all (\ (parent, child) -> elem child (getChildren g parent))
                     $ zip path (tail path)
 
 simplePaths :: Graph -> Int -> Node -> [Path]
-simplePaths gr k n = [x | x <- [subsequences gr], isPath gr x]
+simplePaths gr k n = [xs | xs <- subsequences (map fst gr), isPath gr xs, length xs == k + 1, head xs == n]
