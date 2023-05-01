16d8e3c2b8dd942b6e3a94f066993916cf8b12be
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-01 11:39:04 +0300

    Update Task8.hs

--- For_Home_01/Task8.hs
+++ For_Home_01/Task8.hs
@@ -5,14 +5,9 @@ main = do
     print $ snail 10 3 2 == 8
     print $ snail 100 20 5 == 7
     print $ snail 5 10 3 == 1
- 
+    print $ snail 5 5 2 == 1
+
 snail :: Int -> Int -> Int -> Int
-snail heightOfColumns distancePerDay slidesDownForNight = if distancePerDay >= heightOfColumns
-     then 1
-     else helper heightOfColumns distancePerDay slidesDownForNight 0 0
- where
-    helper heightOfColumns distancePerDay slidesDownForNight days distance
-     | heightOfColumns == distance + distancePerDay = (days + 1)
-    helper heightOfColumns distancePerDay slidesDownForNight days distance = if heightOfColumns <= distance
-         then days 
-         else helper heightOfColumns distancePerDay slidesDownForNight (days + 1) (distance + distancePerDay - slidesDownForNight)
+snail column day night
+ | day <= night = 1
+ | otherwise = ceiling $ fromIntegral (column - night) / fromIntegral (day - night)
