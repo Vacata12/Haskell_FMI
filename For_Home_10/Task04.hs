15da487835123b3643e505233e0d53d2eadc2784
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-02 17:47:38 +0300

    Update Task04.hs

--- For_Home_10/Task04.hs
+++ For_Home_10/Task04.hs
@@ -10,9 +10,25 @@ type Tree = [(Int, [Int])]
 t :: Tree
 t = [(1,[2,3,4]),(2,[5,6]),(3,[7]),(4,[8,9]),(5,[]),(6,[10]),(7,[]),(8,[]),(9,[]),(10,[])]
 
-findUncles :: Tree -> Int -> [Int]
-findUncles tree x = [fst node | node <- tree, isUncle node x]
+getFather :: Tree -> Int -> Int
+getFather tree node = head [x | (x, y) <- tree, elem node y, length y > 0]
 
-isUncle :: (Int, [Int]) -> Int -> Bool
-isUncle (node, children) x = any (\child -> x `elem` snd (tree !! (child - 1))) children
-  where tree = sortOn fst tree
+getBrothers :: Tree -> Int -> [Int]
+getBrothers [] _ = []
+getBrothers tree node = helper tree node
+ where
+     helper [] _ = []
+     helper copyTree node
+      | elem (getFather tree node) (snd (head copyTree)) = snd $ head copyTree
+      | otherwise = helper (tail copyTree) node
+
+findUncles :: Tree -> Int -> [Int]
+findUncles tree node = filter (/= getFather tree node) [z | z <- y]
+ where
+     y = getBrothers tree node
+
+
+
+
+
+
