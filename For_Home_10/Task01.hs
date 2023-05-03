b1b9a3535a703b5ec33edbb25e631b78cd620aee
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-03 19:36:12 +0300

    Update Task01.hs

--- For_Home_10/Task01.hs
+++ For_Home_10/Task01.hs
@@ -3,5 +3,10 @@ main = do
     print $ (sumExpr (2+) [0, 1, 2, 3]) 2 -- == 80
     print $ (sumExpr (*0.8) [0, 1, 2, 3, 4, 5]) 10 -- == 4345680.0
 
-sumExpr ::(Num a) => (a -> a) -> [Int] -> (a -> a)
-sumExpr f ys = helper 1 ys
+putIndex :: [Int] -> [(Int, Int)]
+putIndex ys = zip ys [1 ..]
+
+sumExpr :: (Num a) => (a -> a) -> [Int] -> (a -> a)
+sumExpr f ys = \x -> sum [fromIntegral (fst y) * f (x ^ snd y) | y <- putIndex ys]
+
+
