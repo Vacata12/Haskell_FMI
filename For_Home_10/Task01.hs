a1bb8d76eb7fb7270b48d41088f527bdd026abe4
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-04 07:41:40 +0300

    Update Task01.hs

--- For_Home_10/Task01.hs
+++ For_Home_10/Task01.hs
@@ -1,12 +1,9 @@
 main :: IO()
 main = do
-    print $ (sumExpr (2+) [0, 1, 2, 3]) 2 -- == 80
-    print $ (sumExpr (*0.8) [0, 1, 2, 3, 4, 5]) 10 -- == 4345680.0
-
-putIndex :: [Int] -> [(Int, Int)]
-putIndex ys = zip ys [1 ..]
+    print $ (sumExpr (2+) [0, 1, 2, 3]) 2 == 80
+    print $ (sumExpr (*0.8) [0, 1, 2, 3, 4, 5]) 10 == 4345680.0
 
 sumExpr :: (Num a) => (a -> a) -> [Int] -> (a -> a)
-sumExpr f ys = \x -> sum [fromIntegral (fst y) * f (x ^ snd y) | y <- putIndex ys]
+sumExpr f xs = (\x -> sum [ fromIntegral (y) * f (x^i) | (y, i) <- zip xs [1 ..]])
 
 
