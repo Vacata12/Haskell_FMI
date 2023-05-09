9a25581554cbb5c15772cb58be46030daa11868b
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-09 16:20:04 +0300

    Update Task01.hs

--- Homework_02/Task01.hs
+++ Homework_02/Task01.hs
@@ -1,10 +1,10 @@
 main :: IO()
 main = do
-    print $ getNameLengthColor db ((== 'Y'), (> 106))  == [("PrettyWoman",119),("The Man Who Wasn't There",116),("Logan'srun",120),("Empire Strikes Back",111),("Star Trek",132),("Star Trek:Nemesis",116)]
+    print $ getNameLengthColor db ((== 'Y'), (> 106))  == [("Pretty Woman",119),("The Man Who Wasn't There",116),("Logan's run",120),("Empire Strikes Back",111),("Star Trek",132),("Star Trek: Nemesis",116)]
     print $ getNameLengthColor db ((== 'Y'), (> 237))  == []
     print $ getNameLengthColor db ((== 'Y'), (> 238))  == []
-    print $ getNameLengthColor db ((== 'N'), (< 133))  == [("Terms ofEndearment",132)]
-    print $ getNameLengthColor db ((== 'N'), (< 300))  == [("Terms ofEndearment",132)]
+    print $ getNameLengthColor db ((== 'N'), (< 133))  == [("Terms of Endearment",132)]
+    print $ getNameLengthColor db ((== 'N'), (< 300))  == [("Terms of Endearment",132)]
 
 
 getNameLengthColor :: MovieDB -> ((Char -> Bool), (Int -> Bool)) -> [(Title, Length)]
@@ -30,10 +30,11 @@ type MovieExec = (Name, ProducerID, Networth)
 type MovieDB = ([Movie], [Studio], [MovieExec])
 
 studios :: [Studio]
-studios = [("Disney", 199),("USA Entertainm.", 222),("Fox",333),("Paramount", 123),("MGM", 555),("Buzzfeed Entertainm.", 42)]
+studios = [("Disney", 199), ("USA Entertainm.", 222), ("Fox",333), ("Paramount", 123), ("MGM", 555), ("Buzzfeed Entertainm.", 42)]
 movieExecs :: [MovieExec]
-movieExecs = [("George Lucas", 555, 200000000),("Ted Turner", 333,125000000),("Stephen Spielberg", 222, 100000000),("Merv Griffin",199, 112000000),("Calvin Coolidge", 123, 20000000),("ChristianBaesler", 42, 420000)]
+movieExecs = [("George Lucas", 555, 200000000),("Ted Turner", 333,125000000),("Stephen Spielberg", 222, 100000000),("Merv Griffin",199, 112000000),("Calvin Coolidge", 123, 20000000),("Christian Baesler", 42, 420000)]
 movies :: [Movie]
-movies = [("Pretty Woman", 1990, 119, 'Y', "Disney"),("The Man WhoWasn't There", 2001, 116, 'Y', "USA Entertainm."),("Logan's run",1976, 120, 'Y', "Fox"),("Star Wars", 1977, -1, 'N', "Fox"),("Star Wars2", 1977, 238, 'N', "Fox"),("Empire Strikes Back", 1980, 111, 'Y',"Fox"),("Star Trek", 1979, 132, 'Y', "Paramount"),("Star Trek:Nemesis", 2002, 116, 'Y', "Paramount"),("Terms of Endearment", 1983,132, 'N', "MGM"),("The Usual Suspects", 1995, 106, 'Y', "MGM"),("GoneWith the Wind", 1938, 238, 'Y', "MGM"),("Gone With the Wind 2", 1938,238, 'Y', "MGM"),("The Fellowship of the Ring", 2001, -1, 'Y', "USAEntertainm.")]
+movies = [("Pretty Woman", 1990, 119, 'Y', "Disney"),("The Man Who Wasn't There", 2001, 116, 'Y', "USA Entertainm."),("Logan's run",1976, 120, 'Y', "Fox"),("Star Wars", 1977, -1, 'N', "Fox"),("Star Wars 2", 1977, 238, 'N', "Fox"),("Empire Strikes Back", 1980, 111, 'Y', "Fox"),("Star Trek", 1979, 132, 'Y', "Paramount"),("Star Trek: Nemesis", 2002, 116, 'Y', "Paramount"),("Terms of Endearment", 1983, 132, 'N', "MGM"),("The Usual Suspects", 1995, 106, 'Y', "MGM"),("Gone With the Wind", 1938, 238, 'Y', "MGM"),("Gone With the Wind 2", 1938,238, 'Y', "MGM"),("The Fellowship of the Ring", 2001, -1, 'Y', "USA Entertainm.")]
 db :: MovieDB
 db = (movies, studios, movieExecs)
+
