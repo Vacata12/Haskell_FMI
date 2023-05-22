4d0501d6d0911e1cbf46d8a1abc6be6350690b47
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-22 08:55:38 +0300

    Update Task02.hs

--- For_Home_11/Task02.hs
+++ For_Home_11/Task02.hs
@@ -5,4 +5,9 @@ main = do
 data Color = Red | Green | Blue
 data Tree = Empty | Node Color Tree Tree
 
-colorTree = 
+minDepthGreenNode :: Tree -> Int
+minDepthGreenNode Empty = 0
+minDepthGreenNode (Node Green _ _) = 1
+minDepthGreenNode (Node _ l r) = 1 + min (minDepthGreenNode l) (minDepthGreenNode r)
+
+colorTree = Node Blue (Node Red (Node Green Empty Empty) Empty) (Node Red (Node Blue (Node Green Empty Empty) (Node Red Empty Empty)) Empty)
