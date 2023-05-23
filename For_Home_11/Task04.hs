6e1b96df2c5729d75a6c9a3de4c4b83c8c32f508
Vacata12 <ivan.stoynev12@gmail.com>
2023-05-23 12:51:09 +0300

    Update Task04.hs

--- For_Home_11/Task04.hs
+++ For_Home_11/Task04.hs
@@ -9,4 +9,13 @@ data NTree a = T a [(NTree a)]
 t1 = T 1 [T 3 [], T 5 [], T 7 [], T 9 []]
 t2 = T 7 [T 9 [T 5 [], T 2 []]]
 
+getList :: NTree a -> [a]
+getList (T r xs) = r : concatMap getList xs
+
+isGraceful ::(Num a) => NTree a -> Bool
+isGraceful tree = helper (getList tree) (abs (head (head (getList tree)) - head (getList tree)))
+ where
+     helper (x:xs) dif
+      | abs(x - head xs) /= difference = False
+
 
